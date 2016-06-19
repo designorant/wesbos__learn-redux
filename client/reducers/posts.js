80aa3cbed8b40a9f86a6107643e9d0fe035cdaf9
Michał Ordon <michal@tehidea.com>
2016-06-20 00:22:50 +0100

    Update state with a reducer

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -4,9 +4,18 @@
 // 2. Copy of current state
 
 function posts(state = [], action) {
-  console.log('The post will change')
-  console.log(state, action);
-  return state;
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+      console.log('Incrementing Likes!');
+      const i = action.index;
+      return [
+        ...state.slice(0,i), // Before the one we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1), // After the one we are updating
+      ]
+    default:
+      return state;
+  }
 }
 
 export default posts;
