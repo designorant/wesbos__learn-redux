dcd1d08530de768e0ef5bab4988cab2c297438f0
Michał Ordon <michal@tehidea.com>
2016-06-21 23:41:41 +0100

    Make use of reducer composition in comments

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,4 +1,36 @@
+function postComments(state = [], action) {
+  switch(action.type) {
+    case 'ADD_COMMENT':
+      // Return the new state with the new comment
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
+      console.log(state);
+      console.log('Removing a comment');
+      // Return the new state without the deleted comment
+      return [
+        // From the start to the one we want to delete
+        ...state.slice(0, action.index),
+        // After the deleted one to the end
+        ...state.slice(action.index + 1)
+      ]
+    default:
+      return state;
+  }
+  return state;
+}
+
 function comments(state = [], action) {
+  if(typeof action.postId !== 'undefined') {
+    return {
+      // Take the current state
+      ...state,
+      // Overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state;
 }
 
